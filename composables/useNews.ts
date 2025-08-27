6b52ad70ccb75c3d04355d0283df6989625108c0
youhei usami <usao9110@icloud.com>
2025-08-27 10:44:38 +0400

    8/27

--- composables/useNews.ts
+++ composables/useNews.ts
@@ -1,2 +1,12 @@
 import { news } from "~/data/news";
 import type { News } from "~/types/news";
+
+const byDateDesc = (a: News, b: News) =>
+  new Date(b.date).getTime() - new Date(a.date).getTime()
+
+export function useNews() {
+  const all = computed(() => [...news].sort(byDateDesc))
+  const latest = (n = 3) => all.value.slice(0, n)
+  const findBySlug = (slug: string) => all.value.find(n => n.slug === slug)
+  return { all, latest, findBySlug }
+}
