812ab0e8bbd2183ca3cc4d87b6aad79d1a9cb343
youhei usami <usao9110@icloud.com>
2025-09-03 01:48:28 +0400

    9/3

---
+++ data/works.ts
@@ -0,0 +1,21 @@
+import type { Work } from "../types/works";
+
+export const works: Work[] = [
+  {
+    id: 1,
+    title: 'デザート作品',
+    img: '',
+    category: 'patissier',
+    date: '',
+    tags: ['cake', 'dessert']
+  },
+  {
+    id: 2,
+    title: 'ToDo App',
+    img: '',
+    category: 'programming',
+    slug: 'todo-app',
+    date: '',
+    tags: ['App',]
+  }
+]
---
+++ types/works.ts
@@ -0,0 +1,13 @@
+
+export interface Work {
+  id: number | string,
+  title: string,
+  img?: string,
+  slug?: string,
+  category: 'patissier' | 'programming' | 'design' | 'hobby',
+  subTitle?: string,
+  date?: string | Date,
+  description?: string,
+  tags?: string[],
+  extarnalURL?: string,
+}
