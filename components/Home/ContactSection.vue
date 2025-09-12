e22fabb73a02a552a45d06b860f6aadf2ae74462
youhei usami <usao9110@icloud.com>
2025-09-12 11:18:23 +0400

    9/12

--- components/Home/ContactSection.vue
+++ components/Home/ContactSection.vue
@@ -38,20 +38,36 @@
               <label class="form__label" for="email">
                 メールアドレス<span class="req" aria-hidden="true">必須</span>
               </label>
-              <input 
-                class="form__control" 
-                type="email" 
-                id="email" 
+              <input
+                class="form__control"
+                :class="{ 'is-invalid' : !!errors.email}"
+                type="email"
+                id="email"
                 name="your-email"
-                
+                v-model.trim="form.email"
+                required
+                autocomplete="email"
+                :aria-invalid="!!errors.email"
+                :aria-describedby="errors.email ? 'err-email' : undefined"
               />
+              <p v-if="errors.email" class="form__error" id="err_email" role="alert">{{ errors.email }}</p>
             </div>
 
             <div class="form__row">
-              <label class="form__label" for="email">
+              <label class="form__label" for="topic">
                 お問い合せ内容<span class="req" aria-hidden="true">必須</span>
               </label>
-              <select class="form__control" name="" id="">
+              <select 
+                class="form__control"
+                :class="{ 'is-invalid' : !!errors.topic}"
+                name="topic" 
+                id="topic"
+                v-model.trim="form.topic"
+                required
+                autocomplete="topic"
+                :aria-invalid="!!errors.topic"
+                :aria-describedby="errors.topic ? 'err_topic' : undefined"
+                >
                 <option value="-----">------------</option>
                 <option value="webcontents">Web制作</option>
                 <option value="appcreate">アプリ開発</option>
@@ -61,17 +77,26 @@
                 <option value="photo">写真撮影</option>
                 <option value="other">その他</option>
               </select>
+              <p v-if="errors.topic" class="form__error" id="err_topic" role="alert">{{ errors.topic }}</p>
             </div>
 
             <div class="form__row form_row--textarea">
               <label class="form__label" for="message">
                 内容<span class="req" aria-hidden="true">必須</span>
               </label>
-                <textarea class="form__control"
+                <textarea 
+                  class="form__control"
+                  :class="{ 'is-invalid' : !!errors.message}"
                   id="message"
                   name="your-message"
                   rows="6"
+                  v-model.trim="form.message"
+                  required
+                  autocomplete="message"
+                  :aria-invalid="!!errors.message"
+                  :aria-describedby="errors.message ? 'err_message' :undefined"
                 ></textarea>
+                <p v-if="errors.message" class="form__error" id="err_message" role="alert">{{ errors.message }}</p>
             </div>
 
             <Button class="form__submit" buttonText="submit" link="/usami/news">
