c67953a2706eb4c80941a0155446ccc7be02b897
youhei usami <usao9110@icloud.com>
2025-11-08 20:37:28 +0400

    11/8

--- components/common/NavButton.vue
+++ components/common/NavButton.vue
@@ -1,47 +1,47 @@
 <template>
-          <ul class="header_items">
-            <li class="header_item">
-              <NuxtLink to="/#about" class="header_link">About</NuxtLink>
-            </li>
-            <li class="header_item">
-              <NuxtLink to="/#service" class="header_link">Service</NuxtLink>
-            </li>
-            <li class="header_item">
-              <NuxtLink to="/#news" class="header_link">News</NuxtLink>
-            </li>
-            <li class="header_item">
-              <NuxtLink to="/#works" class="header_link">Works</NuxtLink>
-            </li>
-            <li class="header_item">
-              <NuxtLink to="/#contact" class="header_link-contact"
-                ><span>Contact</span>
-              </NuxtLink>
-            </li>
-          </ul>
+  <ul class="header_items">
+    <li class="header_item">
+      <NuxtLink to="/#about" class="header_link">About</NuxtLink>
+    </li>
+    <li class="header_item">
+      <NuxtLink to="/#service" class="header_link">Service</NuxtLink>
+    </li>
+    <li class="header_item">
+      <NuxtLink to="/#news" class="header_link">News</NuxtLink>
+    </li>
+    <li class="header_item">
+      <NuxtLink to="/#works" class="header_link">Works</NuxtLink>
+    </li>
+    <li class="header_item">
+      <NuxtLink to="/#contact" class="header_link-contact"
+        ><span>Contact</span>
+      </NuxtLink>
+    </li>
+  </ul>
 </template>
 
 <script setup lang="ts">
-import { useRouter } from 'vue-router';
+import { useRouter } from "vue-router";
 import { onMounted, watch } from "vue";
 /**===================================================================================================================
  * 
  ===================================================================================================================**/
-const route = useRoute()
+const route = useRoute();
 
 onMounted(() => {
-watch(
-  () => route.hash,
-  (hash) => {
-    if(!hash) return
+  watch(
+    () => route.hash,
+    (hash) => {
+      if (!hash) return;
 
-    requestAnimationFrame(() => {
-      const el = document.querySelector(hash) as HTMLElement | null
-      el?.scrollIntoView({behavior: 'smooth'})
-    })
-  }
-)
-})
- //------------------------------------------------------------------------------------------------------------
+      requestAnimationFrame(() => {
+        const el = document.querySelector(hash) as HTMLElement | null;
+        el?.scrollIntoView({ behavior: "smooth" });
+      });
+    }
+  );
+});
+//------------------------------------------------------------------------------------------------------------
 // 引数
 //------------------------------------------------------------------------------------------------------------
 //------------------------------------------------------------------------------------------------------------
@@ -115,7 +115,7 @@ watch(
   transition-property: width;
   background-color: #cba96d;
 }
-.header_link-contact{
+.header_link-contact {
   border: 1px solid #666666;
   border-radius: 4px;
   display: block;
@@ -125,7 +125,7 @@ watch(
   position: relative;
   color: #141414;
   background-color: transparent;
-  padding: 4px 24px;
+  padding: 4px 20px;
   margin: 5px auto;
 }
 
@@ -134,22 +134,21 @@ watch(
   z-index: 9999;
 }
 
-.header_link-contact::after{
+.header_link-contact::after {
   content: "";
   position: absolute;
   left: -10px;
-  top: 0;
+  top: -10;
   height: 490%;
-  width: 150%;
+  width: 160%;
   background: #e9af53;
   transition: all 0.5s ease-in-out;
   transform: translateX(-110%) translateY(-22%) rotate(45deg);
 }
-.header_link-contact:hover span { color:#fff; }
-.header_link-contact:hover::after{
+.header_link-contact:hover span {
+  color: #fff;
+}
+.header_link-contact:hover::after {
   transform: translateX(-6%) translateY(-20%) rotate(45deg);
 }
-
-
-
 </style>
