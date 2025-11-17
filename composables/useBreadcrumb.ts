413b59b5ca02c0b647bfdd1ff3a7e10375bb5fe3
youhei usami <usao9110@icloud.com>
2025-11-17 09:55:54 +0400

    11/17

--- composables/useBreadcrumb.ts
+++ composables/useBreadcrumb.ts
@@ -3,23 +3,20 @@ import { computed } from "vue";
 type Crumb = {
   label: string;
   to?: string;
-}
+};
 
-export const useBreadcrumb = ( page: string, title?: string ) => {
-  const map : Record<string, Crumb[]> = {
-    news:[{label: 'News', to: '/news'}],
-    works:[{label: 'Works', to: '/works'}],
-    service:[{label: 'Service', to: '/service'}],
-    about:[{label: 'About', to: '/about'}],
-    contact:[{label: 'Contact', to:'/#contact'}]
-  }
+export const useBreadcrumb = (page: string, ...extra: Array<Crumb>) => {
+  const map: Record<string, Crumb[]> = {
+    news: [{ label: "News", to: "/news" }],
+    works: [{ label: "Works", to: "/works" }],
+    service: [{ label: "Service", to: "/service" }],
+    about: [{ label: "About", to: "/about" }],
+    contact: [{ label: "Contact", to: "/#contact" }],
+  };
 
   const crumbs = computed<Crumb[]>(() => {
-    const base = map[page] || []
-    return [
-      ...base,
-      ...(title ? [{label: title}] : [])
-    ]
-  })
-  return { crumbs }
-}
+    const base = map[page] || [];
+    return [...base, ...extra];
+  });
+  return { crumbs };
+};
